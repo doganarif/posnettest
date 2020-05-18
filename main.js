4720c2faa74804cd2ded7701b204fb9c233ab4ca
Arif Dogan <arifdogan@8black.co>
2020-05-19 00:18:57 +0300

    test

---
+++ main.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const data = `<posnetRequest>
+<mid>6706598320</mid>
+<tid>67543744</tid>
+<oosRequestData>
+<posnetid>28568</posnetid>
+<XID>YKB_0000080603143050</XID>
+<amount>5696</amount>
+<currencyCode>TL</currencyCode>
+<installment>04</installment>
+<tranType>Sale</tranType>
+<cardHolderName>Arif Dogan</cardHolderName>
+<ccno>4506347027911094</ccno>
+<expDate>2412</expDate>
+<cvc>000</cvc>
+</oosRequestData>
+</posnetRequest>`;
+
+axios
+  .post(
+    `https://setmpos.ykb.com/3DSWebService/YKBPaymentService?xmldata=${data}`
+  )
+  .then(res => {
+    console.log(res.data);
+  });
